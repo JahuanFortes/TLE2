ea5188b4433ce5737deeb7c2eaaf89ee9e7ed379
AnnyXi <114316992+Anny-Xi@users.noreply.github.com>
2024-05-22 17:37:41 +0200

    Change event click to mousedown/up .
    change click event for start and end of recognition function to mousedown/up event.

--- speechToText.js
+++ speechToText.js
@@ -1,31 +1,45 @@
-const speechButton = document.getElementById('startButton');
+const speechButton = document.getElementById('voiceButton');
+
+const speechStart = document.getElementById('startSpeech');
+const speechEnd = document.getElementById('endSpeech');
+
 const outputText = document.getElementById('input');
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition);
 
 recognition.lang = 'nl-NL';
 recognition.interimResults = true;
+recognition.continuous = true;
 
+speechEnd.style.display = "none";
 
-recognition.onresult = function(event) {
+recognition.onresult = function (event) {
     const transcript = event.results[0][0].transcript;
     console.log(transcript);
     outputText.innerText = " " + transcript;
 }
 
-recognition.onend = function() {
-    speechButton.style.display = "block";
+recognition.onend = function () {
+    speechStart.style.display = "block";
+    speechEnd.style.display = "none";
 }
 
-speechButton.addEventListener('click', function() {
+speechButton.addEventListener('mousedown', function () {
     if (recognition.recording) {
-        setTimeout(() => {
-            recognition.stop();
-        }, 9000);
-        speechButton.style.display = "block";
+        speechStart.style.display = "block";
+        speechEnd.style.display = "none";
+
     } else {
         recognition.start();
-        speechButton.style.display = "none";
+        speechStart.style.display = "none";
+        speechEnd.style.display = "block";
         outputText.textContent = "...";
     }
 });
+
+speechButton.addEventListener('mouseup', function () {
+
+    recognition.stop();
+    speechStart.style.display = "block";
+    speechEnd.style.display = "none";
+})
 
